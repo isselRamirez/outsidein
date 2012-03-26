9bb1a9fdb84175d61cf9db8234e4a678377a7a0c
unknown <Issel@.(none)>
2012-03-26 09:02:51 -0600

    all steps passed

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(params[:book])
+    if @book.save
+      redirect_to books_path
+    end
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outsidein::Application.config.secret_token = '3af91e47ffe9f8949bacd9d982cf2f8ec014a9da02e981834843f851b302570546bc865f4d346eae5a022f27957fae0a1e5c940a8890d51d9a961c3dc86cda2e'
